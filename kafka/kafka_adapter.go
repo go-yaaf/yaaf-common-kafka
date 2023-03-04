5092b4e0faf19f530bd6427e3ee0ef6bf4082579
Motty Cohen <motty.cohen@gmail.com>
2023-03-04 22:25:44 +0100

    skeleton

--- kafka/kafka_adapter.go
+++ kafka/kafka_adapter.go
@@ -1,49 +1,66 @@
-package kafka
+package kafkaadp
 
 import (
+	"context"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/go-yaaf/yaaf-common/config"
 	. "github.com/go-yaaf/yaaf-common/messaging"
+	"net/url"
+	"strings"
+	"time"
 )
 
 type kafkaAdapter struct {
-	//client *k.AdminClient
+	client      *kafka.AdminClient
+	config      *kafka.ConfigMap
+	subscribers map[string]chan bool
 }
 
 // NewKafkaMessageBus factory method for Kafka IMessageBus implementation
-//
 // param: URI - represents the redis connection string in the format of: kafka://host:port
 func NewKafkaMessageBus(URI string) (mq IMessageBus, err error) {
 
-	//uri, err := url.Parse(URI)
-	//
-	//if err != nil {
-	//	return nil, err
-	//}
+	uri, err := url.Parse(URI)
+	if err != nil {
+		return nil, fmt.Errorf("parsing URI: %s failed: %s", URI, err.Error())
+	}
 
-	//conf := &k.ConfigMap{}
-	//conf.SetKey("bootstrap.servers", "")
-	//
-	//"bootstrap.servers":     a.kafkaBrokerEndpoints,
-	//	"go.batch.producer":     true,
-	//	"request.required.acks": "1",
-	//	"message.max.bytes":     GetBaseConfig().KafkaMaxMessageSizeMb() << (10 * 2),
-	//
-	//if client, er := k.NewAdminClient(conf); err != nil {
-	//	return nil, er
-	//} else {
-	//	adapter := &kafkaAdapter{client: client}
-	//	return adapter, nil
-	//}
-	return nil, nil
+	conf := &kafka.ConfigMap{}
+	conf.SetKey("bootstrap.servers", uri.Host)
+	conf.SetKey("go.batch.producer", true)
+	conf.SetKey("request.required.acks", "1")
+	if strings.ToLower(config.Get().LogLevel()) == "debug" {
+		conf.SetKey("debug", "protocol")
+	}
+
+	if client, er := kafka.NewAdminClient(conf); err != nil {
+		return nil, er
+	} else {
+		adapter := &kafkaAdapter{
+			client:      client,
+			config:      conf,
+			subscribers: make(map[string]chan bool)}
+		return adapter, nil
+	}
 }
 
 // Ping Test connectivity for retries number of time with time interval (in seconds) between retries
-func (r *kafkaAdapter) Ping(retries uint, intervalInSeconds uint) error {
+func (r *kafkaAdapter) Ping(retries uint, intervalInSeconds uint) (err error) {
 
-	// TODO: implement PING method
-	return nil
+	for i := 0; i < int(retries); i++ {
+		if _, err = r.client.ClusterID(context.Background()); err != nil {
+			time.Sleep(time.Second * time.Duration(intervalInSeconds))
+		} else {
+			return nil
+		}
+	}
+	return fmt.Errorf("could not establish connection after %d retries: %s", retries, err)
 }
 
+// Close connection and free client resources
 func (r *kafkaAdapter) Close() error {
+	r.client.Close()
 	return nil
 }
 
--- kafka/kafka_message_bus.go
+++ kafka/kafka_message_bus.go
@@ -1,7 +1,13 @@
-package kafka
+package kafkaadp
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/go-yaaf/yaaf-common/config"
+	"github.com/go-yaaf/yaaf-common/entity"
+	"github.com/go-yaaf/yaaf-common/logger"
 	"time"
 
 	_ "encoding/json"
@@ -15,49 +21,70 @@ import (
 
 // Publish messages to a channel (topic)
 func (r *kafkaAdapter) Publish(messages ...IMessage) error {
-
-	if len(messages) == 0 {
-		return nil
-	}
-
-	// Create topics map
-	//topicsMap := make(map[string]*pubsub.Topic)
-	//for _, message := range messages {
-	//	if _, ok := topicsMap[message.Topic()]; !ok {
-	//		if topic, err := r.getOrCreateTopic(message.Topic()); err != nil {
-	//			return err
-	//		} else {
-	//			topicsMap[message.Topic()] = topic
-	//		}
-	//	}
-	//}
-
-	// Send messages to topic
-	//for _, message := range messages {
-	//	if bytes, er := messageToRaw(message); er != nil {
-	//		return er
-	//	} else {
-	//		if topic, ok := topicsMap[message.Topic()]; ok {
-	//			_ = topic.Publish(context.Background(), &pubsub.Message{Data: bytes})
-	//		}
-	//	}
-	//}
-	return nil
+	return fmt.Errorf("publush is not supported in Apache Kafka implementation, use Producer.Publish()")
 }
 
 // Subscribe on topics
 func (r *kafkaAdapter) Subscribe(factory MessageFactory, callback SubscriptionCallback, subscriberName string, topics ...string) (subscriptionId string, error error) {
 
-	if len(topics) != 1 {
-		return "", fmt.Errorf("only one topic allawed in this implementation")
+	bootstrapServers, err := r.config.Get("bootstrap.servers", "localhost:9200")
+	if err != nil {
+		return "", err
 	}
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":        bootstrapServers,
+		"broker.address.family":    "v4",
+		"group.id":                 subscriberName,
+		"session.timeout.ms":       60000,
+		"auto.offset.reset":        "earliest",
+		"enable.auto.offset.store": true,
+	})
 
-	return "", nil
+	err = c.SubscribeTopics(topics, nil)
+	sId := entity.ID()
+	sChannel := make(chan bool)
+	r.subscribers[sId] = sChannel
+
+	go func() {
+		run := true
+
+		for run {
+			select {
+			case sig := <-sChannel:
+				fmt.Printf("Caught termination signal %v: terminating", sig)
+				run = false
+			default:
+				ev := c.Poll(100)
+				if ev == nil {
+					continue
+				}
+
+				switch e := ev.(type) {
+				case *kafka.Message:
+					message := factory()
+					if er := json.Unmarshal(e.Value, message); er != nil {
+						return
+					}
+					go callback(message)
+				default:
+					// fmt.Printf("Ignored %v", e)
+				}
+			}
+		}
+		logger.Error("closing consumer")
+		_ = c.Close()
+	}()
+	return sId, nil
 }
 
 // Unsubscribe with the given subscriber id
 func (r *kafkaAdapter) Unsubscribe(subscriptionId string) bool {
-	return false
+	if ch, ok := r.subscribers[subscriptionId]; !ok {
+		return false
+	} else {
+		ch <- true
+		return true
+	}
 }
 
 // Push Append one or multiple messages to a queue
@@ -72,14 +99,31 @@ func (r *kafkaAdapter) Pop(factory MessageFactory, timeout time.Duration, queue 
 
 // CreateProducer creates message producer for specific topic
 func (r *kafkaAdapter) CreateProducer(topicName string) (IMessageProducer, error) {
-	//if topic, err := r.getOrCreateTopic(topicName); err != nil {
-	//	return nil, err
-	//} else {
-	//	return &kafkaProducer{topicName: topicName, topic: topic}, nil
-	//}
 
-	r.client.CreateTopics()
-	return nil, nil
+	producer, err := kafka.NewProducer(r.config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Kafka Producer: %s", err.Error())
+	}
+
+	var md *kafka.Metadata
+
+	// Try to connect to kafka for 1 minute, exit if connection failed
+	if md, err = producer.GetMetadata(&topicName, true, 60*1000); err != nil {
+		return nil, fmt.Errorf("failed to connect to Kafka broker: %s", err.Error())
+	}
+
+	// Ensure topic exists and create it if not exists
+	if md != nil {
+		if _, ok := md.Topics[topicName]; !ok {
+			if er := r.createTopics(topicName); er != nil {
+				return nil, er
+			}
+		}
+	}
+
+	kp := &kafkaProducer{topicName: topicName, producer: producer, deliveryChan: make(chan kafka.Event)}
+	kp.run()
+	return kp, nil
 }
 
 // endregion
@@ -87,11 +131,14 @@ func (r *kafkaAdapter) CreateProducer(topicName string) (IMessageProducer, error
 // region Producer actions ---------------------------------------------------------------------------------------------
 
 type kafkaProducer struct {
-	topicName string
+	topicName    string
+	producer     *kafka.Producer
+	deliveryChan chan kafka.Event
 }
 
 // Close producer does nothing in this implementation
 func (p *kafkaProducer) Close() error {
+	p.producer.Close()
 	return nil
 }
 
@@ -102,7 +149,60 @@ func (p *kafkaProducer) Publish(messages ...IMessage) error {
 		return nil
 	}
 
+	for _, msg := range messages {
+		if err := p.publish(msg); err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+// Publish single message to a producer channel (topic)
+func (p *kafkaProducer) publish(message IMessage) error {
+
+	// Set partition ket by message addressee
+	topicPartition := kafka.TopicPartition{Topic: &p.topicName, Partition: kafka.PartitionAny}
+	var partitionKey []byte = nil
+	if len(message.Addressee()) > 0 {
+		partitionKey = []byte(message.Addressee())
+	}
+
+	// Marshal message to byte array
+	data, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("error marshaling message to Json: %s", err.Error())
+	}
+
+	err = p.producer.Produce(&kafka.Message{
+		TopicPartition: topicPartition,
+		Value:          data,
+		Key:            partitionKey,
+	}, p.deliveryChan)
+
+	if err != nil {
+		return fmt.Errorf("error publish message to Kafka topic: %s: %s", p.topicName, err.Error())
+	}
+	return nil
+}
+
+// listen to Kafka delivery channel to track delivery errors
+func (p *kafkaProducer) run() {
+	pump := func() {
+		for {
+			select {
+			case e := <-p.deliveryChan:
+				m := e.(*kafka.Message)
+
+				if m.TopicPartition.Error != nil {
+					logger.Error("Error delivering message via KAFKA: %s", m.TopicPartition.Error)
+				} else {
+					logger.Debug("Message delivered to KAFKA topic %s [%d] at offset %v",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+				}
+			}
+		}
+	}
+	go pump()
 }
 
 // endregion
@@ -110,13 +210,31 @@ func (p *kafkaProducer) Publish(messages ...IMessage) error {
 // region PRIVATE SECTION ----------------------------------------------------------------------------------------------
 
 // Get topic or create it if not exists
-//func (r *kafkaAdapter) getOrCreateTopic(topicName string) (topic *pubsub.Topic, error error) {
-//
-//}
+func (r *kafkaAdapter) createTopics(topics ...string) (err error) {
 
-// Get reference to existing subscription or create new topic if not exists
-//func (r *kafkaAdapter) getOrCreateSubscription(topic *pubsub.Topic, subscriberName string) (*pubsub.Subscription, error) {
-//
-//}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+	defer func() {
+		if cancel != nil {
+			cancel()
+		}
+	}()
+
+	specs := make([]kafka.TopicSpecification, 0)
+	for _, topic := range topics {
+		spec := kafka.TopicSpecification{
+			Topic:         topic,
+			NumPartitions: config.Get().TopicPartitions(),
+		}
+		specs = append(specs, spec)
+	}
+
+	_, err = r.client.CreateTopics(ctx, specs, kafka.SetAdminValidateOnly(false))
+
+	if err != nil {
+		return fmt.Errorf("error creating KAFKA topic: %s", err.Error())
+	} else {
+		return nil
+	}
+}
 
 // endregion
